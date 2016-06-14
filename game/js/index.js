a4af2cfc78947cb67962140aefb408aa25f7c3a3
swegwaz <wberney@connect.kellettschool.com>
2016-06-14 16:12:14 +0800

    game upload

---
+++ game/js/index.js
@@ -0,0 +1,45 @@
+function game() {
+
+var question_1 = prompt('Who created the double-sided lightsaber?');
+
+if (question_1 === "Exar Kun") {
+  alert('Correct!');
+}
+
+else {
+  alert('You are incorrect.  You are not the King/Queen of STAR WARS!  You are not fit for this quiz.  Please leave this page.');
+}
+
+var question_2 = prompt('What planet was Obi-Wan Kenobi born on?');
+
+if (question_2 === "Stewjon") {
+  alert('Correct!');
+}
+ else {
+   alert('You are incorrect.  You are not the King/Queen of STAR WARS!  You are not fit for this quiz.  Please leave this page.');
+}
+
+var question_3 = prompt('What type of lightsaber style does Anakin Skywalker follow?')
+if (question_3 === "Ataru") {
+  alert('Correct!');
+}
+else { 
+  alert('You are incorrect.  You are not the King/Queen of STAR WARS!  You are not fit for this quiz.  Please leave this page.');
+}
+var question_4 = prompt('When was Darth Zannah born?')
+if (question_4 === "1010 BBY") {
+  alert('Correct!');
+}
+else {
+  alert('You are incorrect.  You are not the King/Queen of STAR WARS!  You are not fit for this quiz.  Please leave this page.');
+}
+
+var question_5 = prompt('What cybernetic body part did Darth Malak posses?')
+if (question_5 === "Prosthetic jaw") {
+  alert('Congratulations! You have obtained the title of True Star Wars Fan.  Please tell all your friends about this game and also make sure to check out my website jediwarrior101.github.io');
+}
+else {
+  alert('Incorrect.  You were very close. Thanks for playing!');
+}
+  
+}
